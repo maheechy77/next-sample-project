d3d800fcbbe95cbe8627ac2da1cfe59911c73a14
maheechy77 <maheechy96@gmail.com>
2022-09-26 01:42:24 +0600

    2nd build

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
+import style from "./index.module.css";
 
 export default function Home({ countries }) {
   return (
     <div>
       <h1>Next Js with Graphql</h1>
-      {countries.map((country) => (
-        <p key={country.code}>{country.name}</p>
-      ))}
+      <div className={style.flex}>
+        {countries.map((country, index) => (
+          <div className={style.flexChild}>
+            {index + 1} <p key={country.code}>{country.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
